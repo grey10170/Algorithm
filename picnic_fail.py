8ec41af722c15e44c56e7d6a42bf4e2927d947b6
grey10170 <grey10170@gmail.com>
2021-04-20 15:08:09 +0900

    1일차

---
+++ picnic_fail.py
@@ -0,0 +1,28 @@
+import sys
+rl = lambda: sys.stdin.readline()
+n_case = int(rl().strip())
+
+def n_pair(node, edge):
+    cnt = 0
+    if len(node) == 1:
+        return 0
+    if len(node) == 0:
+        return 1
+    del_node = node[0]
+    for pair in edge:
+        if del_node in pair:
+            new_node = []
+            for no in node:
+                if no not in pair:
+                    new_node.append(no)
+            new_edge = []
+            for new_pair in edge:
+                if len(set.intersection(set(pair), set(new_pair))) == 0:
+                    new_edge.append(new_pair)
+            cnt += n_pair(new_node, new_edge)
+    return cnt
+for i in range(n_case):
+    n_node, n_edge =list(map(int, rl().strip().split(" ")))
+    edge = list(map(int, rl().strip().split(" ")))
+    edge = list(zip(edge[::2], edge[1::2]))
+    print(n_pair(list(range(n_node)), edge))
